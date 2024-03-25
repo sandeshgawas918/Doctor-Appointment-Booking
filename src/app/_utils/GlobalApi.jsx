f948c584a5d25e21e8f724db678287dc68aacd3a
sandesh gawas <sandeshgawas918@gmail.com>
2024-03-25 19:16:15 +0530

    new changes

--- src/app/_utils/GlobalApi.jsx
+++ src/app/_utils/GlobalApi.jsx
@@ -3,7 +3,8 @@ const { default: axios } = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api",
+  // baseURL: "http://localhost:1337/api",
+  baseURL: "https://doctor-appointment-booking-strapi.onrender.com/api",
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
